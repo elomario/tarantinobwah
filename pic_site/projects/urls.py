74cc06fb99202942ab15e27bffb83fdcdbb25ebf
elomario <omarmater@yahoo.fr>
2014-05-14 11:40:37 +0200

    Projects url html render
    rendered projects url with HTML page, needs still to get info on each
    project

--- pic_site/projects/urls.py
+++ pic_site/projects/urls.py
@@ -3,5 +3,9 @@ from django.conf.urls import patterns, url
 from projects import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+	# ex: /projects/myproject/
+    url(r'^(?P<projects_title>\d+)/$', views.detail, name='detail'),
+    # ex: /projects/myproject/results/
+    url(r'^(?P<projects_title>\d+)/results/$', views.results, name='results'),
 )
