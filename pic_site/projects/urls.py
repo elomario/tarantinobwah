05f441d5bedd3b67845d4d5c64cc145251e1d302
elomario <omarmater@yahoo.fr>
2014-05-18 15:22:08 +0200

    Added url: /projects/project_id/ details
    url added to see projects details

--- pic_site/projects/urls.py
+++ pic_site/projects/urls.py
@@ -3,9 +3,10 @@ from django.conf.urls import patterns, url
 from projects import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+    # ex: /projects/
+	url(r'^$', views.index, name='index'),
 	# ex: /projects/myproject/
-    url(r'^(?P<projects_title>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<projects_id>\d+)/$', views.detail, name='detail'),
     # ex: /projects/myproject/results/
     url(r'^(?P<projects_title>\d+)/results/$', views.results, name='results'),
 )
--- pic_site/projects/views.py
+++ pic_site/projects/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from projects.models import Project
+
 # Create your views here.
 
 def index(request):
@@ -12,10 +13,9 @@ def index(request):
     })
     return HttpResponse(template.render(context))
 
-def detail(request, projects_title):
-    latest_projects_list = Project.objects.order_by('-title')[:5]
-    output = ', '.join([p.title for p in latest_projects_list])
-    return HttpResponse(output)
+def detail(request, projects_id):
+	project = get_object_or_404(Project, pk=projects_id)
+	return render(request, 'projects/description.html', {'project': project})
 	
 def results(request, projects_title):
     return HttpResponse("You're looking at the results of project %s." % projects_title)
