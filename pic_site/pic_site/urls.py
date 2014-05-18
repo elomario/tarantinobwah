4df79c5521cbca36c389f83717ed84084dba7a21
elomario <omarmater@yahoo.fr>
2014-05-18 18:31:46 +0200

    Home and presentation URL added
    added basic html templates for home page

--- pic_site/pic_site/urls.py
+++ pic_site/pic_site/urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from pic_site import views
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pic_site.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+	url(r'^home/', 'pic_site.views.home', name='home'),
+	url(r'^presentation/', views.presentation, name='presentation'),
+	url(r'^actualites/', views.actualites, name='actualites'),
+	url(r'^shell/', views.shell, name='shell'),
+	
 	url(r'^projects/', include('projects.urls')),
 	url(r'^events/', include('events.urls')),
     url(r'^admin/', include(admin.site.urls)),
